e94a3fa6a08fc0b6ca3258c719c643feb55aea36
L1S22BSSE0020 <L1S22BSSE0020@ucp.com>
2025-06-03 09:12:50 +0500

    new code

---
+++ setup.sh
@@ -0,0 +1,11 @@
+git init
+git branch -m develop
+git config --local user.name "Haider"
+git config --local user.email "l1s22bsse0020@ucp"
+git config --local core.editor notepad
+mkdir ./src ./css ./bin ./assets ./node_module
+touch ./src/index.html ./css/index.css ./.gitignore
+
+git remote add origin https://github.com/L1S22BSSE0020/S2Week10Gitaction.git
+
+echo "Repositoy initilized"
